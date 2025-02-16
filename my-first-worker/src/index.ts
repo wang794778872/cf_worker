5a3a126d6e5417acfa18f976b847f76d0624a235
wang794778872 <13516853103@163.com>
2025-02-16 20:07:11 +0800

    1.1

---
+++ my-first-worker/src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		console.log(request.url)
+		console.log(request.text())		
+		console.log(request.json())
+		console.log(request.method)
+		console.log(request.body)
+		console.log(request.headers)
+
+		return new Response(request.url);
+	},
+} satisfies ExportedHandler<Env>;
